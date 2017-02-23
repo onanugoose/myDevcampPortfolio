ee276c4e62cb538f6be0d91290a1349a1deb32da
Jordan Hudgens <jordan@devcamp.com>
2017-02-23 08:41:45 +0000

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    WarsawViewTool::Renderer.copyright 'Fred Warsaw', 'All rights reserved'
+  end
 end

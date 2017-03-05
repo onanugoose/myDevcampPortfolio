362226219be0c3780266959f7f092a5c09380b02
Jordan Hudgens <jordan@devcamp.com>
2017-03-05 08:21:14 +0000

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,20 +1,29 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
     @portfolio_items = Portfolio.by_position
   end
-  
-  def angular
-     @angular_portfolio_items = Portfolio.angular
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
   end
-  
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
@@ -26,14 +35,11 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-    
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -42,32 +48,31 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def show
-     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def destroy
-    # Performs lookup
-    @portfolio_item = Portfolio.find(params[:id])
-    
-    # Destroys/deletes records
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
-    # Redirects
+
+    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
-  
+
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
-                                      technologies_attributes:[:name]
-                                      )
+                                      technologies_attributes: [:name]
+                                     )
   end
-  
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
